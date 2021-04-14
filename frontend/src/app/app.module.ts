9f0d3e9d547fec5ecad12937bfc545b523193d50
Alexander Soto <alexandersotoch@gmail.com>
2021-04-14 15:40:54 -0500

    feat(frontend): Añade vista landing y refactoriza componentes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,26 +8,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
-import { MaterialModule } from './mareial.module';
+import { MaterialModule } from './material/material.module';
 
-import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
-import { GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
-import { SignInSocialComponent } from './resources/components/sign-in-social/sign-in-social.component';
-import { LandingComponent } from './resources/view/landing/landing.component';
-
+import {
+  SocialLoginModule,
+  SocialAuthServiceConfig,
+} from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+} from 'angularx-social-login';
+import { SignInSocialComponent } from './components/sign-in-social/sign-in-social.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 const fbLoginOptions = {
-  scope: 'pages_messaging,pages_messaging_subscriptions,email,pages_show_list,manage_pages',
+  scope:
+    'pages_messaging,pages_messaging_subscriptions,email,pages_show_list,manage_pages',
   return_scopes: true,
-  enable_profile_selector: true
+  enable_profile_selector: true,
 };
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignInSocialComponent,
-    LandingComponent
-  ],
+  declarations: [AppComponent, SignInSocialComponent, LandingComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -39,7 +41,10 @@ const fbLoginOptions = {
     ReactiveFormsModule,
   ],
   providers: [
-    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'fill' },
+    },
     {
       provide: 'SocialAuthServiceConfig',
       useValue: {
@@ -47,12 +52,15 @@ const fbLoginOptions = {
         providers: [
           {
             id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider('565209211119569', fbLoginOptions)
-          }
-        ]
+            provider: new FacebookLoginProvider(
+              '565209211119569',
+              fbLoginOptions
+            ),
+          },
+        ],
       } as SocialAuthServiceConfig,
-    }
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
